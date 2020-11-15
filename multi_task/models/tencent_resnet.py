2d02cf313e8571f5340c8f086ad1668f9da5aded
nohssiwi <toappledore@163.com>
2020-11-15 21:31:16 +0100

    model:encoder+decoder

--- multi_task/models/tencent_resnet.py
+++ multi_task/models/tencent_resnet.py
@@ -78,6 +78,28 @@ class ResNet(nn.Module):
             self.in_planes = planes * block.expansion
         return nn.Sequential(*layers)
 
+    def spatial_pyramid_pool(self,previous_conv, num_sample, previous_conv_size, out_pool_size):
+        '''
+        previous_conv: a tensor vector of previous convolution layer
+        num_sample: an int number of image in the batch
+        previous_conv_size: an int vector [height, width] of the matrix features size of previous convolution layer
+        out_pool_size: a int vector of expected output size of max pooling layer
+        
+        returns: a tensor vector with shape [1 x n] is the concentration of multi-level pooling
+        '''    
+        for i in range(len(out_pool_size)):
+            h_wid = int(math.ceil(previous_conv_size[0] / out_pool_size[i]))
+            w_wid = int(math.ceil(previous_conv_size[1] / out_pool_size[i]))
+            h_pad = int(math.floor((h_wid*out_pool_size[i] - previous_conv_size[0] + 1)/2))
+            w_pad = int(math.floor((w_wid*out_pool_size[i] - previous_conv_size[1] + 1)/2))
+            maxpool = nn.MaxPool2d((h_wid, w_wid), stride=(h_wid, w_wid), padding=(h_pad, w_pad))
+            x = maxpool(previous_conv)
+            if(i == 0):
+                spp = x.view(num_sample,-1)
+            else:
+                spp = torch.cat((spp,x.view(num_sample,-1)), 1)
+        return spp
+
     def forward(self, x, mask):
         out = F.relu(self.bn1(self.conv1(x)))
         out = self.layer1(out)
@@ -87,6 +109,7 @@ class ResNet(nn.Module):
         out = F.avg_pool2d(out, 4)
         # out = out.view(out.size(0), -1)
         #out = self.linear(out)
+        out = self.spatial_pyramid_pool(out, 2, [int(out.size(2)), int(out.size(3))], [4,2,1])
         return out, mask
 
 
@@ -130,7 +153,6 @@ class TencentDecoder(nn.Module):
         # ppm_out = torch.cat(ppm_out, 1)
 
         # x = self.conv_last(ppm_out)
-        x = self.spatial_pyramid_pool(conv_out, 2, [int(conv_out.size(2)),int(conv_out.size(3))], [4,2,1])
         # if self.task_type == 'C':
         #     x = nn.functional.log_softmax(x, dim=1)
         x = self.fc1(x)
@@ -143,24 +165,4 @@ class TencentDecoder(nn.Module):
         return x, mask
 
 
-    def spatial_pyramid_pool(self,previous_conv, num_sample, previous_conv_size, out_pool_size):
-        '''
-        previous_conv: a tensor vector of previous convolution layer
-        num_sample: an int number of image in the batch
-        previous_conv_size: an int vector [height, width] of the matrix features size of previous convolution layer
-        out_pool_size: a int vector of expected output size of max pooling layer
-        
-        returns: a tensor vector with shape [1 x n] is the concentration of multi-level pooling
-        '''    
-        for i in range(len(out_pool_size)):
-            h_wid = int(math.ceil(previous_conv_size[0] / out_pool_size[i]))
-            w_wid = int(math.ceil(previous_conv_size[1] / out_pool_size[i]))
-            h_pad = int(math.floor((h_wid*out_pool_size[i] - previous_conv_size[0] + 1)/2))
-            w_pad = int(math.floor((w_wid*out_pool_size[i] - previous_conv_size[1] + 1)/2))
-            maxpool = nn.MaxPool2d((h_wid, w_wid), stride=(h_wid, w_wid), padding=(h_pad, w_pad))
-            x = maxpool(previous_conv)
-            if(i == 0):
-                spp = x.view(num_sample,-1)
-            else:
-                spp = torch.cat((spp,x.view(num_sample,-1)), 1)
-        return spp
+
