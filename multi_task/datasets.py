229ca15af5888ac984d60f442bf14e922e305390
nohssiwi <toappledore@163.com>
2020-12-22 12:06:26 +0100

    fix bug

--- multi_task/datasets.py
+++ multi_task/datasets.py
@@ -53,8 +53,8 @@ def get_dataset(params, configs):
         return train_loader, train_dst, val_loader, val_dst
     
     if 'tencent' in params['dataset']:
-        train_dst = TENCENT(root=configs['tencent']['path'], is_transform=True, split='train', img_size=(configs['tencent']['img_rows'], configs['tencent']['img_cols']), augmentations=None)
-        val_dst = TENCENT(root=configs['tencent']['path'], is_transform=True, split='val', img_size=(configs['tencent']['img_rows'], configs['tencent']['img_cols']), augmentations=None)
+        train_dst = TENCENT(root=configs['tencent']['path'], type='train',patch_number = 10)
+        val_dst = TENCENT(root=configs['tencent']['path'], type='val', patch_number = 10)
 
         # train_loader = torch.utils.data.DataLoader(train_dst, batch_size=params['batch_size'], shuffle=True, num_workers=4, collate_fn=my_collate)
         # val_loader = torch.utils.data.DataLoader(val_dst, batch_size=params['batch_size'], num_workers=4, collate_fn=my_collate)
