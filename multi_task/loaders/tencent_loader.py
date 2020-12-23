a981a3889f45aabb8ca4563a86e1cd2aab380b5a
nohssiwi <toappledore@163.com>
2020-12-23 22:41:11 +0100

    divide dataset train val test

--- multi_task/loaders/tencent_loader.py
+++ multi_task/loaders/tencent_loader.py
@@ -4,7 +4,7 @@ import csv
 import scipy.misc as m
 import numpy as np
 import torchvision.transforms as transforms
-
+import pandas as pd
 from PIL import Image
 
 
@@ -12,62 +12,90 @@ from torch.utils import data
 
 
 class TENCENT(data.Dataset):
-    def __init__(self, root, type, patch_size = 10, is_transform = True):
+    def __init__(self, root, type, patch_size, is_transform = True):
         self.root = root
         self.type = type
         self.is_transform = is_transform
         # self.augmentations = augmentations
         self.patch_size = patch_size
-        self.files = {
-            'train' : [],
-            'val' : []
-        }
-        self.labels = {
-            'train' : [],
-            'val' : []
-        }
+        # self.files = {
+        #     'train' : [],
+        #     'val' : []
+        # }
+        # self.labels = {
+        #     'train' : [],
+        #     'val' : []
+        # }
 
         dataset = {}
-        all_scores_files = glob.glob(root + '/subjective_scores_v2/*.csv')
-        for file in all_scores_files:
-            # print(file)
-            dimension = file.split('/')[-1].split('_')[1].split('.')[0]
-            # print(dimension)
-            key = ''
-            if dimension == 'overall' :
-                key = 'o'
-            elif dimension == 'color' :
-                key = 'h'
-            elif dimension == 'colorfulness' :
-                key = 'c'
-            elif dimension == 'fineness' :
-                key = 'f'
-            with open(file, 'r') as f:
-                reader = csv.reader(f)
-                header = next(reader)
-                for row in reader :
-                    dis = self.distribution(row[2:22])
-                    dis = dis.reshape(-1, 1)
-                    index = row[1].find('img')
-                    filename = row[1][:index-1] + '/' + row[1][index:]
-                    if row[1] in dataset:
-                        dataset[row[1]]['labels'][key] = torch.tensor(dis)
-                    else:
-                        dataset[row[1]] = {
-                            'filename' : filename,
-                            'labels' : {key : torch.tensor(dis)}
-                        }
-        dataset = [dataset[x] for x in dataset]
+        # all_scores_files = glob.glob(root + '/subjective_scores_v2/*.csv')
+
+        data = pd.csv_reader(root + '/subjective_scores_v2/all.csv)
+
+        dataset = {
+            'train' : [], 
+            'val' : [],
+            'test' : []
+        }
+
+        train_df = data[data['type']=='train']
+        train = []
+        for index, row in data.iterrows():
+            item = {
+                'filename' : row['filename'],
+                'label_h' : distribution(row['h_0':'h_19']),
+                'label_c' : distribution(row['c_0':'c_19']),
+                'label_f' : distribution(row['f_0':'f_19']),
+                'label_o' : distribution(row['o_0':'o_19'])
+            }
+            if row['type'] == 'train' :
+                dataset['train'].append(item)
+            elif row['type'] == 'val' :
+                dataset['val'].append(item)
+            else :
+                dataset['test'].append(item)
+
+        self.dataset = dataset
+
+        # for file in all_scores_files:
+        #     # print(file)
+        #     dimension = file.split('/')[-1].split('_')[1].split('.')[0]
+        #     # print(dimension)
+        #     key = ''
+        #     if dimension == 'overall' :
+        #         key = 'o'
+        #     elif dimension == 'color' :
+        #         key = 'h'
+        #     elif dimension == 'colorfulness' :
+        #         key = 'c'
+        #     elif dimension == 'fineness' :
+        #         key = 'f'
+        #     with open(file, 'r') as f:
+        #         reader = csv.reader(f)
+        #         header = next(reader)
+        #         for row in reader :
+        #             dis = self.distribution(row[2:22])
+        #             dis = dis.reshape(-1, 1)
+        #             index = row[1].find('img')
+        #             filename = row[1][:index-1] + '/' + row[1][index:]
+        #             if row[1] in dataset:
+        #                 dataset[row[1]]['labels'][key] = torch.tensor(dis)
+        #             else:
+        #                 dataset[row[1]] = {
+        #                     'filename' : filename,
+        #                     'labels' : {key : torch.tensor(dis)}
+        #                 }
+        # dataset = [dataset[x] for x in dataset]
         # print(dataset)
 
         # divide dataset
         # total: 1091 train: 872 val: 219
-        for i in range(0, 872):
-            self.files['train'].append(dataset[i]['filename'])
-            self.labels['train'].append(dataset[i]['labels'])
-        for i in range(872, 1091):
-            self.files['val'].append(dataset[i]['filename'])
-            self.labels['val'].append(dataset[i]['labels'])
+        # for i in range(0, 872):
+        #     self.files['train'].append(dataset[i]['filename'])
+        #     self.labels['train'].append(dataset[i]['labels'])
+        # for i in range(872, 1091):
+        #     self.files['val'].append(dataset[i]['filename'])
+        #     self.labels['val'].append(dataset[i]['labels'])
 
         # print(self.files['train'])
         # print(self.files['val'])
@@ -84,17 +112,18 @@ class TENCENT(data.Dataset):
 
 
     def __len__(self):
-        return len(self.files[self.type])
+        return len(self.dataset[self.type])
 
     def __getitem__(self, index):
-        img_path = self.files[self.type][index]
-        label_h = self.labels[self.type][index]['h']
-        label_c = self.labels[self.type][index]['c']
-        label_f = self.labels[self.type][index]['f']
-        label_o = self.labels[self.type][index]['o']
+        filename = self.dataset[self.type][index]['filename']
+        label_h = self.dataset[self.type][index]['label_h']
+        label_c = self.dataset[self.type][index]['label_c']
+        label_f = self.dataset[self.type][index]['label_f']
+        label_o = self.dataset[self.type][index]['label_o']
         # print(self.root + '/original_images/' + img_path)
         # img = m.imread(self.root + '/original_images/' + img_path)
-        img = Image.open(self.root + '/original_images/' + img_path).convert('RGB')
+        img = Image.open(self.root + '/original_images/' + filename).convert('RGB')
+    
         if self.is_transform :
             img = self.transform_img(img)
         
