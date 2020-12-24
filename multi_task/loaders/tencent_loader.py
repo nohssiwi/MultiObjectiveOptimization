74e35be0080858e820b48fac288dcb99d22c15f8
nohssiwi <toappledore@163.com>
2020-12-24 11:14:28 +0100

    [L0] details

--- multi_task/loaders/tencent_loader.py
+++ multi_task/loaders/tencent_loader.py
@@ -18,16 +18,6 @@ class TENCENT(data.Dataset):
         self.is_transform = is_transform
         # self.augmentations = augmentations
         self.patch_size = patch_size
-        # self.files = {
-        #     'train' : [],
-        #     'val' : []
-        # }
-        # self.labels = {
-        #     'train' : [],
-        #     'val' : []
-        # }
-
-        # all_scores_files = glob.glob(root + '/subjective_scores_v2/*.csv')
 
         data = pd.read_csv(root + '/subjective_scores_v2/all.csv')
 
@@ -54,50 +44,6 @@ class TENCENT(data.Dataset):
 
         self.dataset = dataset
 
-        # for file in all_scores_files:
-        #     # print(file)
-        #     dimension = file.split('/')[-1].split('_')[1].split('.')[0]
-        #     # print(dimension)
-        #     key = ''
-        #     if dimension == 'overall' :
-        #         key = 'o'
-        #     elif dimension == 'color' :
-        #         key = 'h'
-        #     elif dimension == 'colorfulness' :
-        #         key = 'c'
-        #     elif dimension == 'fineness' :
-        #         key = 'f'
-        #     with open(file, 'r') as f:
-        #         reader = csv.reader(f)
-        #         header = next(reader)
-        #         for row in reader :
-        #             dis = self.distribution(row[2:22])
-        #             dis = dis.reshape(-1, 1)
-        #             index = row[1].find('img')
-        #             filename = row[1][:index-1] + '/' + row[1][index:]
-        #             if row[1] in dataset:
-        #                 dataset[row[1]]['labels'][key] = torch.tensor(dis)
-        #             else:
-        #                 dataset[row[1]] = {
-        #                     'filename' : filename,
-        #                     'labels' : {key : torch.tensor(dis)}
-        #                 }
-        # dataset = [dataset[x] for x in dataset]
-        # print(dataset)
-
-        # divide dataset
-        # total: 1091 train: 872 val: 219
-        # for i in range(0, 872):
-        #     self.files['train'].append(dataset[i]['filename'])
-        #     self.labels['train'].append(dataset[i]['labels'])
-        # for i in range(872, 1091):
-        #     self.files['val'].append(dataset[i]['filename'])
-        #     self.labels['val'].append(dataset[i]['labels'])
-
-        # print(self.files['train'])
-        # print(self.files['val'])
-        # print(len(self.labels['train']))
-        # print(self.labels['val'])
 
     def distribution(self, row):
         dis = np.zeros(5)
@@ -118,19 +64,11 @@ class TENCENT(data.Dataset):
         label_c = self.dataset[self.type][index]['label_c']
         label_f = self.dataset[self.type][index]['label_f']
         label_o = self.dataset[self.type][index]['label_o']
-        # print(self.root + '/original_images/' + img_path)
-        # img = m.imread(self.root + '/original_images/' + img_path)
         img = Image.open(self.root + '/original_images/' + filename).convert('RGB')
     
         if self.is_transform :
             img = self.transform_img(img)
-        
-        
-        # matplotlib.pyplot.imread
-        # if self.augmentations is not None:
-        #     img = self.augmentations(np.array(img, dtype=np.uint8))
-        
-        # if self.is_transform:
+
 
         return img, label_h, label_c, label_f, label_o
 
