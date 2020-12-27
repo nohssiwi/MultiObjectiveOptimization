183c434afe283230f5e1ddefc5ff567fe97ad11e
nohssiwi <toappledore@163.com>
2020-12-27 21:35:44 +0100

    [L0] multi-patch

--- multi_task/datasets.py
+++ multi_task/datasets.py
@@ -16,13 +16,11 @@ def global_transformer():
                                transforms.Normalize((0.1307,), (0.3081,))])
 
 def my_collate(batch):
-    data  = []
-    for item in batch :
-        patches = item[0]
-        for patch in patches :
-            data.append(patch)
-    data = np.array(data)
-    data = torch.from_numpy(data).float()
+    for i, item in enumerate(batch) :
+        if (i==0) :
+            data = item[0]
+        else :
+            data = torch.cat((data, item[0]), 0)
     h = [item[1] for item in batch]
     h = torch.stack(h)
     c = [item[2] for item in batch]
