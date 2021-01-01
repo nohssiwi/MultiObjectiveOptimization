3611f8bc56e334fb7b8a80c2cec257df3e7d325b
nohssiwi <toappledore@163.com>
2021-01-01 03:10:07 +0100

    [L0] fix bug

--- multi_task/5foldcv.py
+++ multi_task/5foldcv.py
@@ -21,32 +21,31 @@ def _5foldcv() :
         'crop_or_pad' : False
     }
 
-    plcc_list = []
-    epoch_list = []
-    identifier = ''
-    for i in range(0, 5) :
-        print(i+1, ' fold')
-        identifier, plcc, epoch = train_multi_task(params, i+1)
-        plcc_list.append(plcc)
-        epoch_list.append(epoch)
-    print(plcc_list)
-    print(epoch_list)
-    best_plcc = max(plcc_list)
-    index = plcc_list.index(best_plcc)
-    best_epoch = epoch_list[index]
+    # plcc_list = []
+    # epoch_list = []
+    # identifier = ''
+    # for i in range(0, 5) :
+    #     print(i+1, ' fold')
+    #     identifier, plcc, epoch = train_multi_task(params, i+1)
+    #     plcc_list.append(plcc)
+    #     epoch_list.append(epoch)
+    # print(plcc_list)
+    # print(epoch_list)
+    # best_plcc = max(plcc_list)
+    # index = plcc_list.index(best_plcc)
+    # best_epoch = epoch_list[index]
     # test
     params['train'] = False
     params['test'] = True 
-    params['exp_identifier'] = identifier
+    # params['exp_identifier'] = identifier
     print('best :', best_epoch, index + 1)   
-    # params['best_epoch'] = 26
-    # params['best_fold'] = 1
-    # params['exp_identifier'] = 'optimizer=SGD|dropout_prob=0.75|lr=0.001|batch_size=8|img_h=454|img_w=984|patch_size=0|global_patch=False|crop_h=256|crop_w=256|crop_or_pad=True'
+    params['exp_identifier'] = 'optimizer=SGD|dropout_prob=0.75|lr=0.001|batch_size=6|img_h=454|img_w=984|patch_size=6|global_patch=True|crop_h=272|crop_w=590|crop_or_pad=False'
     params['batch_size'] = 1
     print('test')
+    epoch_list = [42, 54, 46, 38, 36]
     for i in range(0, 5) :
         params['best_epoch'] = epoch_list[i]
-        params['best_fold'] = index + 1
+        params['best_fold'] = i + 1
         train_multi_task(params)
     
 if __name__ == '__main__':
